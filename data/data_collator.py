e40dd5b8e13182b52503ad05c3e198d03881799f
Beshoy Fawzy <ge79liq@mytum.de>
2022-11-16 15:22:15 +0100

    bugfix, states were not well-defined for different model type, e.g. POMDP, MDP

--- data/data_collator.py
+++ data/data_collator.py
@@ -1,21 +1,28 @@
 import numpy as np
 import torch
 
+
 class DecisionTransformerDataCollator:
     return_tensors: str = "pt"
-    max_len: int = 20 #subsets of the episode we use for training. This is the context length of the DT
+    # subsets of the episode we use for training. This is the context length of the DT
+    max_len: int = 20
     state_dim: int = 1  # size of state space
     act_dim: int = 3  # size of action space
-    max_ep_len: int = 50 # max episode length in the dataset
+    max_ep_len: int = 50  # max episode length in the dataset
     scale: float = 50  # normalization of rewards/returns
     state_mean: np.array = None  # to store state means
     state_std: np.array = None  # to store state stds
     p_sample: np.array = None  # a distribution to take account trajectory lengths
-    n_traj: int = 0 # to store the number of trajectories in the dataset
+    n_traj: int = 0  # to store the number of trajectories in the dataset
 
-    def __init__(self, dataset) -> None:
+    def __init__(self, dataset, model_name='POMDP') -> None:
         self.act_dim = len(dataset[0]["actions"][0])
-        self.state_dim = len(dataset[0]["observations"][0])
+        if self.model_name == 'POMDP':
+            self.state_dim = 1
+        elif self.model_name == 'MDP':
+            self.state_dim = len(dataset[0]["states"][0])
+        else:
+            raise ValueError
         self.dataset = dataset
         # calculate dataset stats for normalization of states
         states = []
@@ -25,8 +32,9 @@ class DecisionTransformerDataCollator:
             traj_lens.append(len(obs))
         self.n_traj = len(traj_lens)
         states = np.vstack(states)
-        self.state_mean, self.state_std = np.mean(states, axis=0), np.std(states, axis=0) + 1e-6
-        
+        self.state_mean, self.state_std = np.mean(
+            states, axis=0), np.std(states, axis=0) + 1e-6
+
         traj_lens = np.array(traj_lens)
         self.p_sample = traj_lens / sum(traj_lens)
 
@@ -48,21 +56,32 @@ class DecisionTransformerDataCollator:
         )
         # a batch of dataset features
         s, a, r, d, rtg, timesteps, mask = [], [], [], [], [], [], []
-        
+
         for ind in batch_inds:
             # for feature in features (a dictionary):
             feature = self.dataset[int(ind)]
-            #si is a random integer between 0 and 999
+            # si is a random integer between 0 and 999
             si = np.random.randint(0, len(feature["rewards"]) - 1)
 
             # get sequences from dataset
-            s.append(np.array(feature["observations"][si : si + self.max_len]).reshape(1, -1, self.state_dim))
-            a.append(np.array(feature["actions"][si : si + self.max_len]).reshape(1, -1, self.act_dim))
-            r.append(np.array(feature["rewards"][si : si + self.max_len]).reshape(1, -1, 1))
+            if self.model_name == 'POMDP':
+                s.append(np.array(
+                    feature["observations"][si: si + self.max_len]).reshape(1, -1, self.state_dim))
+            elif self.model_name == 'MDP':
+                s.append(np.array(
+                    feature["states"][si: si + self.max_len]).reshape(1, -1, self.state_dim))
+            else:
+                raise ValueError
+            a.append(np.array(
+                feature["actions"][si: si + self.max_len]).reshape(1, -1, self.act_dim))
+            r.append(np.array(feature["rewards"]
+                     [si: si + self.max_len]).reshape(1, -1, 1))
 
-            d.append(np.array(feature["dones"][si : si + self.max_len]).reshape(1, -1))
+            d.append(np.array(feature["dones"]
+                     [si: si + self.max_len]).reshape(1, -1))
             timesteps.append(np.arange(si, si + s[-1].shape[1]).reshape(1, -1))
-            timesteps[-1][timesteps[-1] >= self.max_ep_len] = self.max_ep_len - 1  # padding cutoff
+            timesteps[-1][timesteps[-1] >=
+                          self.max_ep_len] = self.max_ep_len - 1  # padding cutoff
             rtg.append(
                 self._discount_cumsum(np.array(feature["rewards"][si:]), gamma=1.0)[
                     : s[-1].shape[1]   # TODO check the +1 removed here
@@ -70,21 +89,29 @@ class DecisionTransformerDataCollator:
             )
             if rtg[-1].shape[1] < s[-1].shape[1]:
                 print("if true")
-                rtg[-1] = np.concatenate([rtg[-1], np.zeros((1, 1, 1))], axis=1)
+                rtg[-1] = np.concatenate([rtg[-1],
+                                         np.zeros((1, 1, 1))], axis=1)
 
             # padding and state + reward normalization
             tlen = s[-1].shape[1]
-            s[-1] = np.concatenate([np.zeros((1, self.max_len - tlen, self.state_dim)), s[-1]], axis=1)
+            s[-1] = np.concatenate([np.zeros((1, self.max_len -
+                                   tlen, self.state_dim)), s[-1]], axis=1)
             s[-1] = (s[-1] - self.state_mean) / self.state_std
             a[-1] = np.concatenate(
-                [np.ones((1, self.max_len - tlen, self.act_dim)) * -10.0, a[-1]],
+                [np.ones((1, self.max_len - tlen, self.act_dim))
+                 * -10.0, a[-1]],
                 axis=1,
             )
-            r[-1] = np.concatenate([np.zeros((1, self.max_len - tlen, 1)), r[-1]], axis=1)
-            d[-1] = np.concatenate([np.ones((1, self.max_len - tlen)) * 2, d[-1]], axis=1)
-            rtg[-1] = np.concatenate([np.zeros((1, self.max_len - tlen, 1)), rtg[-1]], axis=1) / self.scale
-            timesteps[-1] = np.concatenate([np.zeros((1, self.max_len - tlen)), timesteps[-1]], axis=1)
-            mask.append(np.concatenate([np.zeros((1, self.max_len - tlen)), np.ones((1, tlen))], axis=1))
+            r[-1] = np.concatenate([np.zeros((1,
+                                   self.max_len - tlen, 1)), r[-1]], axis=1)
+            d[-1] = np.concatenate([np.ones((1, self.max_len - tlen))
+                                   * 2, d[-1]], axis=1)
+            rtg[-1] = np.concatenate([np.zeros((1, self.max_len - tlen, 1)),
+                                     rtg[-1]], axis=1) / self.scale
+            timesteps[-1] = np.concatenate(
+                [np.zeros((1, self.max_len - tlen)), timesteps[-1]], axis=1)
+            mask.append(np.concatenate(
+                [np.zeros((1, self.max_len - tlen)), np.ones((1, tlen))], axis=1))
 
         s = torch.from_numpy(np.concatenate(s, axis=0)).float()
         a = torch.from_numpy(np.concatenate(a, axis=0)).float()
